aa3d2322070080f19a4ccaa1d45dcc61194857fb
Thyrix <thyrixyang@gmail.com>
2021-02-27 16:31:08 +0800

    rename folder

---
+++ codeforces/cf1430d.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAXN = 1e6;
+char ss[MAXN];
+
+int main()
+{
+
+    int T, N;
+    cin >> T;
+    for (int Case = 0; Case < T; Case++)
+    {
+        cin >> N;
+        scanf("%s", ss);
+        }
+
+    return 0;
+}
