8e27fae0f3f50f0d2c50bdf4e888b3219fc1d03f
Thyrix <thyrixyang@gmail.com>
2021-03-28 12:14:30 +0800

    zhousai

---
+++ leetcode/tmp/a.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int numDifferentIntegers(string word)
+    {
+        set<string> a;
+        string t = "";
+        int lz = 0;
+        for (int i = 0; i < word.length(); i++)
+        {
+            char c = word[i];
+            if (c >= '0' && c <= '9')
+            {
+                if (c == '0' && t.length() == 0)
+                {
+                    lz = 1;
+                }
+                else
+                {
+                    t.push_back(c);
+                }
+            }
+            else
+            {
+                if (t.length() > 0 || lz == 1)
+                {
+                    if (t.length() > 0)
+                    {
+                        a.insert(t);
+                    }
+                    else
+                    {
+                        a.insert("0");
+                    }
+                }
+                lz = 0;
+                t = "";
+            }
+        }
+        if (t.length() > 0)
+        {
+            a.insert(t);
+        }
+        else if (lz > 0)
+        {
+            a.insert("0");
+        }
+        return a.size();
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
---
+++ leetcode/tmp/d.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+
+    float evaluate(int pf, int pn)
+    {
+        float res = 0;
+        if (pf % pn == 0)
+        {
+            float c = pf / pn;
+            return log((float)c) * pn;
+        }
+        else
+        {
+            int c1 = pf / pn;
+            int c2 = c1 + 1;
+            int nc2 = pf % pn;
+            int nc1 = pn - nc2;
+            float res = log((float)c1) * nc1 + log((float)c2) * nc2;
+            return res;
+        }
+    }
+
+    long long f_exp(long long a, long long p, long long pr)
+    {
+        long long ans = 1;
+        while (p > 0)
+        {
+            if (p & 1)
+            {
+                ans = ans * a % pr;
+            }
+            a = a * a % pr;
+            p >>= 1;
+        }
+        return ans;
+    }
+
+public:
+    int maxNiceDivisors(int primeFactors)
+    {
+        long long P = 1e9 + 7;
+        int l = 1;
+        int r = primeFactors;
+        float eps = 1e-9;
+        float lv = evaluate(primeFactors, l);
+        float rv = evaluate(primeFactors, r);
+        while (r - l > 1)
+        {
+            int m = (r + l) / 2;
+            float mv = evaluate(primeFactors, m);
+            float mv2 = evaluate(primeFactors, m + 1);
+            if (mv2 <= mv - eps)
+            {
+                r = m;
+            }
+            else
+            {
+                l = m;
+            }
+        }
+        // cerr << r << endl;
+        if (primeFactors % r == 0)
+        {
+            int c = primeFactors / r;
+            return f_exp(c, r, P);
+        }
+        else
+        {
+            int c1 = primeFactors / r;
+            int c2 = c1 + 1;
+            int nc2 = primeFactors % r;
+            int nc1 = r - nc2;
+            // cerr << c1 << " " << nc1 << " " << c2 << " " << nc2 << endl;
+            long long res = f_exp(c1, nc1, P) * f_exp(c2, nc2, P) % P;
+            return res;
+        }
+    }
+};
+
+int main()
+{
+    Solution sol;
+    int ans = sol.maxNiceDivisors(3);
+    cerr << ans << endl;
+    return 0;
+}
